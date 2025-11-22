44ce751946f6252e88a00c6d57695dbe9ae6c678
Andre Byfield <Andre.Byfield@enlyte.com>
2025-11-22 10:18:05 -0800

    Let's change out auto suspend setting on TF_DEMO_WH to 1 minute.

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "snowflake_warehouse" "tf_warehouse" {
   warehouse_size            = "XSMALL"
   max_cluster_count         = 1
   min_cluster_count         = 1
-  auto_suspend              = 60
+  auto_suspend              = 1
   auto_resume               = true
   enable_query_acceleration = false
   initially_suspended       = true
