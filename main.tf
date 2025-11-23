600806e78cbbabe38052f92bae1c0cd90da66261
Andre Byfield <Andre.Byfield@enlyte.com>
2025-11-22 16:39:28 -0800

    Grant usage on  TF_DEMO_WH to role TF_DEMO_ROLE.

--- main.tf
+++ main.tf
@@ -60,6 +60,17 @@ resource "snowflake_grant_account_role" "grants" {
     user_name         = snowflake_user.tf_user.name
 }
 
+# Grant usage on the warehouse
+resource "snowflake_grant_privileges_to_account_role" "grant_usage_tf_warehouse_to_tf_role" {
+    provider          = snowflake.useradmin
+    privileges        = ["USAGE"]
+    account_role_name = snowflake_account_role.tf_role.name
+    on_account_object {
+        object_type = "WAREHOUSE"
+        object_name = snowflake_warehouse.tf_warehouse.name
+  }
+}
+
 # Grant usage on the database
 resource "snowflake_grant_privileges_to_account_role" "grant_usage_tf_db_to_tf_role" {
     provider          = snowflake.useradmin
